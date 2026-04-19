785c86267314798c89af1727b8a8cd0730ee8654
michihisa hironori <michihisa@michihisanoMacBook-Air.local>
2026-04-19 14:47:45 +0900

    68 mon

--- sort.py
+++ sort.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
 df = pd.read_csv('prob.csv')
-rc = len(df)
-rc_u = len(df[['year', 'no']].drop_duplicates())
+df = df.sort_values(['year', 'no'])
+df.to_csv('prob.csv',index=0)
 
-if rc == rc_u:
+df_u = df.groupby(['year', 'no'])['prob'].count().reset_index()
+df_u = df_u.rename(columns={'prob': 'cnt'})
+df_u = df_u[df_u['cnt'] > 1]
+
+
+if len(df_u) == 0:
     print('OK')
-    df = df.sort_values(['year', 'no'])
-    df.to_csv('prob.csv',index=0)
+else:
+    print(df_u)
